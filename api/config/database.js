b56d4cba24b8f4d8c0a525149c590fe182324c1b
muhammadosama555 <muhammad.osama.31997@gmail.com>
2022-12-28 17:45:46 +0500

    Github upload err

--- api/config/database.js
+++ api/config/database.js
@@ -1,14 +1,1 @@
-const mongoose=require("mongoose")
 
-const connectDatabase=()=>{
-    mongoose.connect(process.env.DB_LOCAL_URI,{
-        useNewUrlParser:true,
-        useUnifiedTopology:true,
-        useCreateIndex:true,
-        
-    }).then(con =>{
-        console.log(`MongoDb Database connected with Host:${con.connection.host}`)
-    })
-}
-
-module.exports= connectDatabase
