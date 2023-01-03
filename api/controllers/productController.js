e703e2ffd48aea6aedc1cc63718978ea1b62b95f
muhammadosama555 <muhammad.osama.31997@gmail.com>
2023-01-03 14:28:41 +0500

    api features

--- api/controllers/productController.js
+++ api/controllers/productController.js
@@ -3,6 +3,7 @@ const Product=require("../models/product.js")
 const ErrorHandler= require("../utils/errorHandler.js")
 const catchAsyncErrors=require('../middlewares/catchAsyncError.js')
 const mongoose = require("mongoose")
+const APIFeatures = require("../utils/apiFeatures.js")
 
 
 //Create new peoduct =>    /api/v1/product/new
@@ -17,10 +18,20 @@ exports.newProduct= catchAsyncErrors( async (req,res,next)=>{
 
 // GET ALL PRODUCTS => /api/v1/products
 exports.getProducts=async (req,res,next)=>{
-         const products= await Product.find()
+    
+       const resPerPage=4;
+       const productCount=await Product.countDocuments();
+
+       const apiFeatures= new  APIFeatures(Product.find(),req.query )
+                          .search()
+                          .filter()
+                          .pagination(resPerPage)
+
+         const products= await   apiFeatures.query;
     res.status(200).json({
-        seccess: true,
+        success: true,
         count:products.length,
+        productCount,
         products
       
     })
