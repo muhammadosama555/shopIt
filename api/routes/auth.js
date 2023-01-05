42ddca0e3b6fe3e7a0dc02e95089caff0ae5bf4e
muhammadosama555 <muhammad.osama.31997@gmail.com>
2023-01-05 18:49:37 +0500

    user route

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -7,13 +7,25 @@ const {
   logout,
   forgotPassword,
   getUserProfile,
+  updatePassword,
+  updateProfile,
+  allUsers,
+  getUserDetails,
+  updateUser,
+  deleteUser,
 } = require("../controllers/authController.js");
-const { isAuthenticatedUser } = require("../middlewares/auth.js");
+const { isAuthenticatedUser, authorizeRoles } = require("../middlewares/auth.js");
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/logout", logout);
 router.post("/password/forgot", forgotPassword);
 router.get("/me", isAuthenticatedUser, getUserProfile);
+router.put("/password/update", isAuthenticatedUser,updatePassword );
+router.put("/me/update", isAuthenticatedUser,updateProfile );
+router.get("/admin/users", isAuthenticatedUser,authorizeRoles('admin'),allUsers );
+router.get("/admin/user/:id", isAuthenticatedUser,authorizeRoles('admin'),getUserDetails );
+router.put("/admin/user/:id", isAuthenticatedUser,authorizeRoles('admin'),updateUser );
+router.delete("/admin/user/:id", isAuthenticatedUser,authorizeRoles('admin'),deleteUser );
 
 module.exports = router;
