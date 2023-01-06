efd30d1f5a990da09eab09e1ea90d2fab3c864cd
muhammadosama555 <muhammad.osama.31997@gmail.com>
2023-01-06 19:28:47 +0500

    review

--- api/routes/product.js
+++ api/routes/product.js
@@ -2,7 +2,7 @@ const express=require('express')
 const router =express.Router()
 
 
-const {getProducts, newProduct, getSingleProduct, updateProduct, deleteProduct} = require("../controllers/productController.js")
+const {getProducts, newProduct, getSingleProduct, updateProduct, deleteProduct, createProductReview, getProductreviews, deleteProductreviews} = require("../controllers/productController.js")
 const { isAuthenticatedUser,authorizeRoles } = require('../middlewares/auth.js')
 
 
@@ -13,6 +13,10 @@ router.post("/product/new",newProduct)
 router.get("/products/admin/:id", getSingleProduct)
 router.put("/products/admin/:id", updateProduct)
 router.delete("/products/admin/:id",  deleteProduct )
+
+router.put("/review", isAuthenticatedUser, createProductReview)
+router.get("/reviews", isAuthenticatedUser, getProductreviews)
+router.delete("/reviews", isAuthenticatedUser, deleteProductreviews)
 
 
 
